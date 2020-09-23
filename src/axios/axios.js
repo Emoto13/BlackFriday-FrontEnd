dd05eb247dda9f49bb8531c146e528197e997d89
Emoto13 <emcho_spassov@abv.bg>
2020-09-23 10:33:02 +0300

    Refactor axios client

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,8 +1,37 @@
 import axios from 'axios';
+import { store } from '../redux/store'
+import { setToken } from '../redux/token/actions';
 
 export const instance = axios.create({
     baseURL: 'http://127.0.0.1:8000/',
-    headers: {
-       'Content-Type': 'application/json'
-    },
    });
+
+export function setInterceptor(axiosInstance=instance, refresh){
+
+    axiosInstance.interceptors.response.use(response => {
+      return response;
+   }, err => {
+        return new Promise((resolve, reject) => {
+            const originalRequest = err.config;
+            if ( err.response.status === 401 && err.config && !err.config.__isRetryRequest )
+            {
+                originalRequest._retry = true;
+            
+                let res = axiosInstance.post('http://127.0.0.1:8000/api/token/refresh/', { refresh })
+                 .then(response => response.json())
+                 .then(json => {
+                     store.dispatch(setToken(json.access, json.refresh))
+                    console.log(json);
+                    return axios(originalRequest);
+                })
+              
+
+              resolve(res);
+          }
+
+
+          return Promise.reject(err);
+      })
+
+   })
+}
